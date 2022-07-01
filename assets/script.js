54d146ee0b7899271e56c6102985b146169c9716
denisdelaki <denisdelaki@gmail.com>
2022-07-01 12:17:38 +0300

    created the card, styled its element and added post and get request

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 const generate = document.getElementById("submitButton");
-const demo=document.getElementById('demo')
+const cards=document.getElementById('cards')
 //event listener
 generate.addEventListener('submit', handleSubmit)
  
@@ -13,21 +13,14 @@ function handleSubmit(e) {
         email: e.target.email.value,
         image: e.target.image.value
     }
-    getCard(card)
+    postCard(card)
     renderCard(card)
     
 }
-//fetch the data
-function getCard() {
-    fetch("http://localhost:3000/card")
-        .then(res => res.json())
-    .then(cards=>cards.forEach(card => {
-       renderCard(card) 
-    }))
-}
+//render the data
+
 function renderCard(card) {
-    let cards = document.createElement('div')
-    cards.id = "cards"
+    
     let myCard = document.createElement('div')
     myCard.id = "card"
     let part1 = document.createElement("div")
@@ -38,7 +31,7 @@ function renderCard(card) {
     cardImage.src = `${card.image}`
     cardImage.id = "image"
     let cardName = document.createElement('p')
-    cardName.textContent = `${card.fname} ${card.lName}`
+    cardName.textContent = `${card.fName} ${card.lName}`
     let contact = document.createElement('h3')
     contact.textContent = "Contact Information"
     let phone = document.createElement('p')
@@ -49,6 +42,7 @@ function renderCard(card) {
     let Email = document.createElement('p')
     let envelope = document.createElement('i')
     envelope.className = "fa-solid fa-envelope";
+    envelope.textContent=`${card.email}`
     Email.appendChild(envelope)
      
     part1.appendChild(cardImage)
@@ -59,6 +53,30 @@ function renderCard(card) {
     part2.appendChild(Email)
     myCard.appendChild(part2)
     cards.appendChild(myCard)
+    
+}
+//fetch the data
+function getCard() {
+  fetch("http://localhost:3000/card")
+    .then((res) => res.json())
+    .then((cards) =>
+      cards.forEach((card) => {
+        renderCard(card);
+      })
+    );
+}
+function postCard(card) {
+    fetch("http://localhost:3000/card", {
+        method: "POST", 
+        headers: {
+            "Content-Type":"application/json"
+        },
+        body:JSON.stringify(card)
+    })
+        .then(res => res.json())
+        .then(card => {
+        console.log(card)
+    })
     
 }
  function iniatialize() {
